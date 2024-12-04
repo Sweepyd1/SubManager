8fb7130567240720d50c1008504d67954aeba803
K1rsN7 <k1rsn7@mail.ru>
2024-12-04 13:25:41 +0300

    Update ALL files

---
+++ README.md
@@ -0,0 +1,113 @@
+
+<div align="center">
+	<img src="./img/banner.jpg">
+    <h1>Subscribe to those who subscribe to you!</h1>
+      <a href="https://github.com/K1rsN7/SubManager/issues">
+		<img src="https://img.shields.io/github/issues/K1rsN7/SubManager?color=C0CBD1&labelColor=1F3B58&style=for-the-badge">
+	</a>
+	<a href="https://github.com/K1rsN7/SubManager/stargazers">
+		<img src="https://img.shields.io/github/stars/K1rsN7/SubManager?color=C0CBD1&labelColor=1F3B58&style=for-the-badge">
+	</a>
+	<a href="./LICENSE">
+		<img src="https://img.shields.io/github/license/K1rsN7/SubManager?color=C0CBD1&labelColor=1F3B58&style=for-the-badge">
+	</a>
+</div>
+<h2 align="center">Project Description</h2>
+<p>SubManager is a powerful and user-friendly Python tool designed to automate the management of subscriptions in GitHub. In today's world where audience interaction is key, it's important to keep your subscriptions up to date and keep up to date with changes in your network of contacts.</p>
+<p>With SubManager, you can easily subscribe users who have subscribed to you and unsubscribe those who have decided to leave you. This makes it much easier to keep your subscription list up-to-date and allows you to focus on engaging with your audience rather than on mundane tasks.</p>
+<p>SubManager's key features include:</p>
+<ul>
+    <li><strong>Cross-platform:</strong> The script is written in Python and runs on a variety of operating systems including Windows, macOS and Linux, making it accessible to a wide audience.</li>
+    <li><strong>Automatic Subscription:</strong> The script automatically subscribes to all users who have subscribed to your account, so you don't miss out on new subscribers.</li>
+    <li><strong>Automatic unsubscribe:</strong> You can easily unsubscribe users who have unsubscribed from you, which helps keep your subscription list clean.</li>
+    <li><strong>Exceptions to the algorithm:</strong> SubManager provides the ability to use two files: <code>ban_list_followers.txt</code> and <code>ban_list_following.txt</code>. These files allow you to add users to a blacklist, excluding them from automatic subscriptions and unsubscriptions, giving you complete control over who you want to leave out of the automated process.</li>
+</ul>
+<h2 align="center">Installation</h2>
+<ol>
+    <li><strong>Clone the repository:</strong>
+        <pre><code>git clone https://github.com/K1rsN7/SubManager.git
+cd SubManager</code></pre>
+    </li>
+    <li><strong>Customize the file <code>main.py</code>:</strong>
+    <p>Open the <code>main.py</code> file and change the following fields to your own:</p>
+    <pre><code>USERNAME = 'YOUR_USERNAME'
+TOKEN = 'YOUR_ACCESS_TOKEN'
+</code></pre>
+    <p>If you are unsure how to generate a personal access token, please refer to the documentation at <a href="./Docs/Generate Token.md">Generate Token.md</a> for detailed instructions.</p>
+</li>
+
+</ol>
+
+<h2 align="center">Running the program</h2>
+<p>To run the program manually, run the following command in the terminal:</p>
+<pre><code>python3 main.py</code></pre>
+
+<h2 align="center">Startup automation</h2>
+<p>To automate the launch of the SubManager script, follow these steps depending on your operating system:</p>
+
+<h3>For Linux or macOS</h3>
+<ol>
+    <li>Open crontab for editing:
+        <pre><code>sudo crontab -e</code></pre>
+    </li>
+    <li>Add the following line to the crontab file:
+        <pre><code>0 */2 * * * /usr/bin/python3 GLOBAL_PATH/main.py</code></pre>
+        <p>Replace <code>GLOBAL_PATH</code> to the full path to the <code>main.py</code> file.</p>
+    </li>
+    <li>Save your changes and exit the editor.</li>
+</ol>
+
+<h3>For Windows</h3>
+<ol>
+    <li><strong>Open the Task Scheduler:</strong>
+        <ul>
+            <li>Press <strong>Windows + R</strong>, then type <code>taskschd.msc</code> and press Enter.</li>
+        </ul>
+    </li>
+    <li><strong>Create a new task:</strong>
+        <ul>
+            <li>In the right pane, select <strong>Create Task</strong>.</li>
+        </ul>
+    </li>
+    <li><strong>Configure the general settings:</strong>
+        <ul>
+            <liEnter the name of the task and a description.li>
+            <li>Make sure that the correct version of Windows is selected in the <strong>Configuration for</strong> field.</li>
+        </ul>
+    </li>
+    <li><strong>Set Trigger:</strong>
+        <ul>
+            <li>Перейдите на вкладку <strong>Триггеры</strong> (<em>Triggers</em>) и нажмите <strong>Создать</strong></li>
+            <li>Select <strong>Schedule</strong></li>
+            <li>Set the frequency to <strong>Every 2 hours</strong>. To do this, you can select “Daily” and then specify the execution interval.</li>
+        </ul>
+    </li>
+    <li><strong>Customize the action:</strong>
+        <ul>
+            <li>Перейдите на вкладку <strong>Действия</strong> (<em>Actions</em>) и нажмите <strong>Создать</strong></li>
+            <li><strong>Select Run Program</strong></li>
+            <li><p><strong>In the “Program or Script”</strong> field, specify the path to the Python interpreter, for example:</p>
+                <pre><code>C:\Path\To\Python\python.exe</code></pre></li>
+            <li><p><strong>In the “Arguments”</strong> field, specify the path to your script:</p>
+                <pre><code>C:\Path\To\Your\Script\main.py</code></pre></li>
+        </ul>
+    </li>
+    <li><strong>Save the task:</strong>
+        <ul>
+            <li><p>Click “OK” to save the task settings.</p></li>
+        </ul>
+    </li>
+</ol>
+
+<h2 align="center">License</h2>
+<p>This project is licensed under the <a href=“./LICENSE”>MIT License</a>. The MIT License is one of the most popular and simple open source licenses. It allows you to:</p>
+<ul>
+    <li><strong>Use:</strong> You can use the project code in your own projects, whether personal or commercial.</li>
+    <li><strong>Modify:</strong> You can modify the project code, adapting it to your needs or improving functionality.</li>
+    <li><strong>Distribute:</strong> You can distribute the original code or your own modifications, and you must specify the authorship of the original project.</li>
+</ul>
+<p>It is important to note that the license does not provide any warranty, and the authors are not responsible for any problems that may occur when using the code.</p>
+
+<h2 align="center">Contacts</h2>
+<p>If you have any questions, suggestions or would like to discuss the project, please contact me via Telegram: <a href=“https://t.me/K1rsN7”>@K1rsN7</a>.</p>
+<p>I'm always happy to hear feedback and suggestions for improving the project. Your support and ideas will help me make SubManager even better!
--- main.py
+++ main.py
@@ -1,129 +1,159 @@
 import requests
+from bs4 import BeautifulSoup
 import logging
+import time
+import os 
+import sys
+import time
 
-# Замените эти значения на свои
+# Replace these values with your own
 USERNAME = 'YOUR_USERNAME'
 TOKEN = 'YOUR_ACCESS_TOKEN'
-GLOBAL_PATH = "YOUR_GLOBAL_PATH"
 
-# URL для API GitHub
+# URL for GitHub API
 BASE_URL = 'https://api.github.com'
-BAN_LIST_FILE_PATH_FOLLOWERS = f'{GLOBAL_PATH}/ban_list_followers.txt'  
-BAN_LIST_FILE_PATH_FOLLOWING = '{GLOBAL_PATH}/ban_list_following.txt' 
 
-# Настройка логирования
+# Upload the ban list
+GLOBAL_PATH = os.getcwd()
+BAN_LIST_FILE_PATH_FOLLOWERS = f'{GLOBAL_PATH}/ban_list_followers.txt'  
+BAN_LIST_FILE_PATH_FOLLOWING = f'{GLOBAL_PATH}/ban_list_following.txt' 
+
+# Characters for process display
+LOADING_CHAR = ['|', '/', '-', '\\']
+
+# Configuring logging
 logging.basicConfig(filename=f'{GLOBAL_PATH}/subscription_manager.log', level=logging.INFO, 
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
-def load_ban_list(file_path):
-    """Загружает бан-лист из файла."""
+def load_ban_list(file_path:str) -> set:
+    """Loads a ban list from a file."""
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
-            return {line.strip() for line in file if line.strip()}
+            return set(line.strip() for line in file if line.strip())
     except FileNotFoundError:
         return set()
 
-def get_followers(ban_list):
-    """Получает список подписчиков с поддержкой постраничной навигации, исключая пользователей из бан-листа."""
-    followers = []
+def get_users_list(ban_list: set, message:str, user_type:str='followers'):
+    """
+    Gets a list of users (subscribers or subscriptions) with support for paginated navigation,
+    excluding users from the ban list.
+
+    Args:
+        ban_list (set): List of users to be excluded
+        message (str): Message to be output in the process
+        user_type (str): Type of users to receive (‘followers’ or ‘following’)
+    Returns:
+        list: User list
+    """
+    if user_type not in ['followers', 'following']:
+        raise ValueError("user_type must be ‘followers’ or ‘following’")
+
+    logging.info(f"Parsing {'followers' if user_type == 'followers' else 'following'}...") 
+    users = []
     page = 1
     while True:
-        url = f'{BASE_URL}/users/{USERNAME}/followers?per_page=100page={page}'
+        sys.stdout.write(f'\r{message} {LOADING_CHAR[page % 4]}')
+        sys.stdout.flush() 
+        url = f'https://github.com/{USERNAME}?tab={user_type}&page={page}'
         try:
             response = requests.get(url)
-            response.raise_for_status()  # Проверка на ошибки
-            current_followers = response.json()
+            response.raise_for_status() # Checking for errors
             
-            if not current_followers:  # Если текущая страница пустая, выходим из цикла
+            # Parsing the HTML code of a page
+            soup = BeautifulSoup(response.text, 'html.parser')
+            current_users = soup.find_all('img', class_='avatar')
+            
+            if not current_users: # If the current page is empty, exit the loop
                 break
             
-            # Фильтруем подписчиков, исключая тех, кто в бан-листе
-            followYOUR_USERNAMEtend(follower['login'] for follower in current_followers if follower['login'] not in ban_list)
-            page += 1  # Переход к следующей странице
+            # Filter users to exclude those on the ban list
+            for user in current_users[1:]:
+                username = user.get('alt')[1:]
+                if username not in ban_list:
+                    users.append(username)
 
-        except requests.exceptions.HTTPError as e:
-            logging.error(f'HTTP Error: {e}')
-            if e.response.status_code == 403:  # Проверка на ошибку 403 (лимит запросов)
-                logging.error('API недоступен временно: превышен лимит запросов.')
-                break  # Выход из цикла при превышении лимита
-        except requests.exceptions.RequestException as e:
-            logging.error(f'Ошибка запроса: {e}')
-            break  # Выход из цикла при других ошибках
-
-    return followers
-
-def get_following(ban_list):
-    """Получает список пользователей, на которых вы подписаны с поддержкой постраничной навигации, исключая пользователей из бан-листа."""
-    following = []
-    page = 1
-    while True:
-        url = f'{BASE_URL}/users/{USERNAME}/following?page={page}'
-        try:
-            response = requests.get(url)
-            response.raise_for_status()  # Проверка на ошибки
-            current_following = response.json()
-            
-            if not current_following:  # Если текущая страница пустая, выходим из цикла
+            # If the number of current users is less than 2, exit the loop
+            if len(current_users) < 2:
                 break
             
-            # Фильтруем подписок, исключая тех, кто в бан-листе
-            following.extend(followed['login'] for followed in current_following if followed['login'] not in ban_list)
-            page += 1  # Переход к следующей странице
+            page += 1  # Go to the next page
+            time.sleep(0.5) # Delay between requests to bypass error 429
 
         except requests.exceptions.HTTPError as e:
-            logging.error(f'HTTP Error: {e}')
-            if e.response.status_code == 403:  # Проверка на ошибку 403 (лимит запросов)
-                logging.error('API недоступен временно: превышен лимит запросов.')
-                break  # Выход из цикла при превышении лимита
-        except requests.exceptions.RequestException as e:
-            logging.error(f'Ошибка запроса: {e}')
-            break  # Выход из цикла при других ошибках
+            logging.error(f'Ошибка HTTP: {e}')
+            break
 
-    return following
+    return users
 
-def follow_user(username):
-    """Подписывается на пользователя."""
+def print_logo() -> None:
+    """Printing of the programme logo"""
+    logo = r"""
+  _____       _    ___  ___                                  
+ /  ___|     | |   |  \/  |                                  
+ \ `--. _   _| |__ | .  . | __ _ _ __   __ _  __ _  ___ _ __ 
+  `--. \ | | | '_ \| |\/| |/ _` | '_ \ / _` |/ _` |/ _ \ '__|
+ /\__/ / |_| | |_) | |  | | (_| | | | | (_| | (_| |  __/ |   
+ \____/ \__,_|_.__/\_|  |_|\__,_|_| |_|\__,_|\__, |\___|_|   
+                                              __/ |          
+                                             |___/           
+_____________________________________________________________          
+    """
+    print(logo)
+
+
+def update_subscription(username:str, isFollowing=False) -> None:
+    """
+    Manages the subscription for the user.
+    Args:
+        username (str): GitHub username
+        isFollowing (bool): True if the user should be followed, False otherwise"""
     url = f'{BASE_URL}/user/following/{username}'
     try:
-        response = requests.put(url, auth=(USERNAME, TOKEN))
+        response = requests.put(url, auth=(USERNAME, TOKEN)) if isFollowing else requests.delete(url, auth=(USERNAME, TOKEN))
         response.raise_for_status()
-        logging.info(f'Подписались на {username}')
+        message = f'{"Subscribed to" if isFollowing else "Unsubscribed from"} {username}'
+        print(message)
+        logging.info(message)
     except requests.exceptions.HTTPError as e:
-        logging.error(f'Не удалось подписаться на {username}: {e}')
+        message = f'Failed {'subscribe' if isFollowing else 'unsubscribe'} from {username}: {e}'
+        print(message)
+        logging.error(message)
     except requests.exceptions.RequestException as e:
-        logging.error(f'Ошибка запроса при подписке на {username}: {e}')
+        message = f'Query error on {'subscriptions' if isFollowing else 'unsubscribes'} for {username}: {e}'
+        print(message)
+        logging.error(message)
 
-def unfollow_user(username):
-    """Отписывается от пользователя."""
-    url = f'{BASE_URL}/user/following/{username}'
-    try:
-        response = requests.delete(url, auth=(USERNAME, TOKEN))
-        response.raise_for_status()
-        logging.info(f'Отписались от {username}')
-    except requests.exceptions.HTTPError as e:
-        logging.error(f'Не удалось отписаться от {username}: {e}')
-    except requests.exceptions.RequestException as e:
-        logging.error(f'Ошибка запроса при отписке от {username}: {e}')
+def manage_subscriptions(ban_list_followers: set, ban_list_following: set) -> None:
+    """
+    Manages the user's subscriptions based on bins lists.
 
-def manage_subscriptions(ban_list_followers, ban_list_following):
-    """Управляет подписками с учетом бан-листов."""
-    followers = get_followers(ban_list_followers)
-    following = get_following(ban_list_following)
+    Args:
+        ban_list_followers (set): The set of users to be unsubscribed from the subscription list.
+        ban_list_following (set): The set of users who should be excluded from the subscription list.
+    """
+    followers = get_users_list(ban_list_followers, message="Get a list of subscribers", user_type='followers')
+    print("\nSubscription list received!")
+    following = get_users_list(ban_list_following, message="Getting a list of subscriptions", user_type='following')
+    print("\nSubscription list received!")
 
-    # Подписываемся на всех, кто на вас подписан
+    # Subscribe to everyone who subscribes to you
     for follower in followers:
         if follower not in following:
-            follow_user(follower)
+            update_subscription(follower, isFollowing=True)
 
-    # Отписываемся от тех, кто не подписан на вас
+    # Unsubscribe from those who are not subscribed to you
     for followed in following:
         if followed not in followers:
-            unfollow_user(followed)
+            update_subscription(followed)
 
 if __name__ == '__main__':
-    
+    print_logo()
+    print("Script Started")
+    logging.info("Script started") 
+
     ban_list_followers = load_ban_list(BAN_LIST_FILE_PATH_FOLLOWERS)  
     ban_list_following = load_ban_list(BAN_LIST_FILE_PATH_FOLLOWING)  
     
-    manage_subscriptions(ban_list_followers, ban_list_following)  
+    manage_subscriptions(ban_list_followers, ban_list_following) 
 
+    print("Script Finished")
